9355ffa0f58062554602849bb8192774597897c3
Michael Safarty <msafarty@acpafl.org>
2024-05-15 09:38:45 -0400

    add dep if run separate

--- pivot_test.R
+++ pivot_test.R
@@ -1,3 +1,3 @@
 # melissa asked for a pivot/summary table; this would be a start and would be more readable once more responses are transformed
-
-ptpa23 %>% group_by(question_num, question_desc, sub_question, category_desc, response) %>% summarize(count=n()) %>% data.frame() %>%  write.csv('testPivot.csv', row.names = F)
+library(dplyr)
+ptpa23 %>% dplyr::group_by(question_num, question_desc, sub_question, category_desc, response) %>% dplyr::summarize(count=n()) %>% data.frame() %>%  write.csv('testPivot.csv', row.names = F)
